d5863f6fb41237908896724d096790a89818eed3
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-11-30 15:26:55 -0300

    cria estrutura docker

--- layouts/default.vue
+++ layouts/default.vue
@@ -80,6 +80,11 @@ export default {
           title: 'Inspire',
           to: '/inspire',
         },
+        {
+          icon: 'mdi-car-side',
+          title: 'Cars',
+          to: '/cars',
+        },
       ],
       miniVariant: false,
       right: true,
