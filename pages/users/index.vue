a3a58d8efe35587603ccaa8b227f2c36f353fd68
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-12-02 17:05:21 -0300

    adiciona layout cbes

---
+++ pages/users/index.vue
@@ -0,0 +1,174 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :page.sync="page"
+      :items-per-page="itemsPerPage"
+      :search="search"
+      hide-default-footer
+      class="elevation-1"
+      @page-count="pageCount = $event"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Usuários</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" dark class="mr-2">
+            <v-icon>mdi-printer</v-icon>
+          </v-btn>
+          <v-btn color="primary" dark>
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-toolbar flat>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Pesquisa"
+            single-line
+            hide-details
+            class="mb-5"
+          ></v-text-field>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.bloqueado="{ item }">
+        <v-simple-checkbox v-model="item.bloqueado"></v-simple-checkbox>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <!-- <v-btn tile small color="primary"
+          ><v-icon small @click="editItem(item)"> mdi-pencil </v-icon></v-btn
+        >
+        <v-btn tile small color="red"
+          ><v-icon small color="white"@click="editItem(item)"> mdi-delete </v-icon></v-btn
+        > -->
+
+        <v-btn icon color="primary"
+          ><v-icon small dark @click="editItem(item)">
+            mdi-pencil
+          </v-icon></v-btn
+        >
+        <v-btn icon color="red"
+          ><v-icon small dark @click="deleteItem(item)">
+            mdi-delete
+          </v-icon></v-btn
+        >
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize"> Reset </v-btn>
+      </template>
+    </v-data-table>
+    <div class="text-center pt-2 pb-5">
+      <v-pagination v-model="page" :length="pageCount"></v-pagination>
+      <!-- <v-text-field
+        :value="itemsPerPage"
+        label="Items per page"
+        type="number"
+        min="-1"
+        max="15"
+        @input="itemsPerPage = parseInt($event, 10)"
+      ></v-text-field> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 5,
+      search: '',
+      headers: [
+        {
+          text: 'Nome',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Email', value: 'email' },
+        { text: 'Telefone', value: 'telefone' },
+        { text: 'Perfil', value: 'perfil' },
+        { text: 'Bloqueado', value: 'bloqueado', align: 'center' },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      desserts: [
+        {
+          name: 'Isaac Newton',
+          email: 'newton@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'capitania',
+          bloqueado: true,
+        },
+        {
+          name: 'Charles Augustin de Coulomb',
+          email: 'coulomb@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'capitania',
+          bloqueado: false,
+        },
+        {
+          name: 'Amedeo Avogadro',
+          email: 'avogadro@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'armador',
+          bloqueado: false,
+        },
+        {
+          name: 'Johann Josef Loschmidt',
+          email: 'loschmidt@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'armador',
+          bloqueado: true,
+        },
+        {
+          name: 'Johann Jakob Balmer',
+          email: 'balmer@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'admin',
+          bloqueado: true,
+        },
+        {
+          name: 'Joseph Stefan',
+          email: 'stefan@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'capitania',
+          bloqueado: false,
+        },
+        {
+          name: 'Ludwig Boltzmann',
+          email: 'boltzmann@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'capitania',
+          bloqueado: true,
+        },
+        {
+          name: 'Johannes Rydberg',
+          email: 'rydberg@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'admin',
+          bloqueado: true,
+        },
+        {
+          name: 'Joseph John Thomson',
+          email: 'thomson@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'admin',
+          bloqueado: true,
+        },
+        {
+          name: 'Max Planck',
+          email: 'planck@domain.com',
+          telefone: '(99) 9999-9999',
+          perfil: 'admin',
+          bloqueado: true,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
